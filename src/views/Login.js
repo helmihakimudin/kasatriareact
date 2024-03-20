55fbb35397091fd2462258451caace3f0467ba72
HH <helmihakimudin@gmail.com>
2024-03-20 10:33:46 +0700

    update button sign in google

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { GoogleLogout } from "react-google-login";
+import { GoogleLogin } from "react-google-login";
+
+const clientId =
+  "373101881628-cm0tnjj01m2q6kc5pjmarkh7c457t3ki.apps.googleusercontent.com";
 
 const Login = () => {
-  return <div>Login</div>;
+  const onSuccess = (res) => {
+    console.log("Login Success: currentUser:", res.profileObj);
+  };
+
+  const onFailure = (res) => {
+    console.log("Login failed: res:", res);
+  };
+
+  const onSignOutSuccess = () => {
+    alert("You have been signed out");
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+      />
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSignOutSuccess}
+      ></GoogleLogout>
+    </div>
+  );
 };
 
 export default Login;
